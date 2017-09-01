29e2b7e6d312d78d2ae651758254bfe6f46e9bcf
André Lindhe Ullvigymnasiet <andre.norberglindhe@koping.se>
2017-09-01 09:41:22 +0200

    Skapade mitt första program
    Skapade ett användarvänligt program.

---
+++ ShowTheClassFirstConsole/ShowTheClassFirstConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ShowTheClassFirstConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Skriv ditt namn:");
+            string namn = Console.ReadLine();
+
+
+            Console.WriteLine("Vad är din adress?");
+            string adress = Console.ReadLine();
+
+
+
+            Console.WriteLine("Ditt namn är alltså: " + namn 
+                              + " och din adress är " + adress
+                              + " Hej då!");
+
+        }
+    }
+}
