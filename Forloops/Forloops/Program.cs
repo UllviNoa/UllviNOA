19807da8d7df16c938d22b5cb6cb44f34f110701
André Lindhe Ullvigymnasiet <andre.norberglindhe@koping.se>
2017-10-02 09:37:56 +0200

    Fragesport + matematik
    Frågesporten och diverse matematikska program.

---
+++ Forloops/Forloops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Forloops
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+            int start = (int)Math.Sqrt(x);
+
+            for (int i = start; i > 1; i--)
+            {
+                if(x % i == 0)
+                {
+                    Console.WriteLine("Inte ett primtal");
+                }
+            }
+
+        }
+    }
+}
