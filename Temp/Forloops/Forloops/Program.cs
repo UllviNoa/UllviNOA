6d1d0f4baf23aa4218c44091a8993192f0e64016
André Lindhe Ullvigymnasiet <andre.norberglindhe@koping.se>
2018-09-12 10:42:54 +0200

    changes...
    YES

---
+++ Temp/Forloops/Forloops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Forloops
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+
+                //KOmmentar!!!!
+                int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+                int start = (int)Math.Sqrt(x);
+
+                for (int i = start; i > 1; i--)
+                {
+                    if (x % i == 0)
+                    {
+                        Console.WriteLine("Delbart med: " + i);
+                    }
+                }
+            }
+
+        }
+    }
+}
